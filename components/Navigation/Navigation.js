5d0a12fc7bc46915e864b69d77c13076c99bde07
Nikhil Vinay Sharma <nikhilvs999@gmail.com>
2021-03-20 11:34:14 +0530

    updated resume

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -19,7 +19,7 @@ const Navigation = () => {
 
             {''}<a key={0}
                      className={cx(styles.right)}
-                     href="https://drive.google.com/file/d/1tEQMh14spsFmtX5DBGvuWpxMs1x1tncI/view?usp=sharing">
+                     href="https://drive.google.com/file/d/1-nVMj6Y4Nm861PE9uymIl9NGmJXKdH0u/view?usp=sharing">
                               Resume
                   </a>{''}
 
